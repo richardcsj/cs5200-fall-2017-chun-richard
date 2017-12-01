b7cd2d90af6a654894d4a61e9a8c3efcb881b8be
richardcsj <richard.chun91@gmail.com>
2017-12-01 15:05:45 +0000

    AppComponent added with modules and routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,75 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routing} from './app.routing';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
+import {UserService} from './services/user.service';
+import {PropertyService} from './services/property.service';
+import {ReviewService} from './services/review.service';
+import {SharedService} from './services/shared.service';
+import {AuthGuard} from './services/auth-guard.service';
+import { AgmCoreModule } from '@agm/core';
+import {GMapsService} from './services/gmaps.service';
+
+
+
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import {Routing} from './app.routing';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CustomerMenuComponent } from './components/menu/customer-menu/customer-menu.component';
+import { OwnerMenuComponent } from './components/menu/owner-menu/owner-menu.component';
+import { AdminMenuComponent } from './components/menu/admin-menu/admin-menu.component';
+import { UserComponent } from './components/admin/user/user.component';
+import { PropertyComponent } from './components/admin/property/property.component';
+import { ReviewComponent } from './components/admin/review/review.component';
+import { UserDetailsComponent } from './components/admin/user/details/details.component';
+import { PropertyDetailsComponent } from './components/admin/property/details/details.component';
+import { ClientComponent } from './components/owner/client/client.component';
+import { OwnerPropertyComponent } from './components/owner/property/property.component';
+import { OwnerReviewComponent } from './components/owner/review/review.component';
+import { SearchComponent } from './components/customer/search/search.component';
+import { ListComponent } from './components/customer/list/list.component';
+import { RentalsComponent } from './components/customer/rentals/rentals.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    MenuComponent,
+    CustomerMenuComponent,
+    OwnerMenuComponent,
+    AdminMenuComponent,
+    UserComponent,
+    PropertyComponent,
+    ReviewComponent,
+    UserDetailsComponent,
+    PropertyDetailsComponent,
+    ClientComponent,
+    OwnerPropertyComponent,
+    OwnerReviewComponent,
+    SearchComponent,
+    ListComponent,
+    RentalsComponent
   ],
   imports: [
     BrowserModule,
-    Routing,
     HttpModule,
-    FormsModule
+    FormsModule,
+    Routing,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBID0s0Lauq-vzzH-V17TCKGaIi3Vd7UCY'
+    })
   ],
-  providers: [],
+  providers: [UserService,PropertyService,ReviewService,SharedService,AuthGuard,GMapsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,46 @@
 import {Routes, RouterModule} from "@angular/router";
 import {ModuleWithProviders} from "@angular/core";
+import {LoginComponent} from "./components/user/login/login.component";
+import {RegisterComponent} from "./components/user/register/register.component";
+import {HomeComponent} from "./components/home/home.component";
+import {ProfileComponent} from "./components/user/profile/profile.component";
+import {MenuComponent} from "./components/menu/menu.component";
+import { UserComponent } from './components/admin/user/user.component';
+import { PropertyComponent } from './components/admin/property/property.component';
+import { ReviewComponent } from './components/admin/review/review.component';
+import { UserDetailsComponent } from './components/admin/user/details/details.component';
+import { PropertyDetailsComponent } from './components/admin/property/details/details.component';
+import { ClientComponent } from './components/owner/client/client.component';
+import { OwnerPropertyComponent } from './components/owner/property/property.component';
+import { OwnerReviewComponent } from './components/owner/review/review.component';
+import { SearchComponent } from './components/customer/search/search.component';
+import { ListComponent } from './components/customer/list/list.component';
+import { RentalsComponent } from './components/customer/rentals/rentals.component';
+import {AuthGuard} from './services/auth-guard.service';
+
+
 
 // Import all other components here
-import { HomeComponent } from './home/home.component';
-
 const APP_ROUTES : Routes = [
   { path : '', component: HomeComponent},
-  { path : 'home', component: HomeComponent}
+  { path : 'login' , component: LoginComponent},
+  { path : 'register' , component:RegisterComponent },
+  { path : 'profile' , component: ProfileComponent, canActivate: [AuthGuard]},
+  { path : 'profile/menu' , component: MenuComponent},
+  { path: 'admin/user', component: UserComponent},
+  { path: 'admin/property', component: PropertyComponent},
+  { path: 'admin/review', component: ReviewComponent},
+  { path: 'admin/user/:userId', component: UserDetailsComponent},
+  { path: 'admin/property/:propertyId', component:PropertyDetailsComponent},
+  { path: 'owner/client',component:ClientComponent},
+  { path: 'owner/client/:clientId',component:ClientComponent},
+  { path: 'owner/property',component:OwnerPropertyComponent},
+  { path: 'owner/property/:propertyId',component:OwnerPropertyComponent},
+  { path: 'owner/review',component:OwnerReviewComponent},
+  { path : 'customer/search',component:SearchComponent},
+  { path : 'customer/list',component:ListComponent},
+  { path : 'customer/type/:type',component:ListComponent},
+  { path : 'customer/rentals',component:RentalsComponent}
 ];
 // Export the routes as module providers
 export const Routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
